bfd6a40747f386b29dca190c15f55767d55a2923
padmashrimallapur <padmashri.mallapur@gmail.com>
2016-04-20 15:08:49 -0400

    Builder Pattern

--- MainClass.java
+++ MainClass.java
@@ -1,12 +1,12 @@
 package com.example;
 
-public class MyClass {
+public class MainClass {
 
 
     public static void main(String[] args) {
         System.out.println("Hi hello java");
 
         NutritionFacts nutritionFacts = new NutritionFacts.Builder(240, 8).calories(8).fat(3).carbohydrate(83).sodium(6).build();
-        System.out.println(nutritionFacts.toString());
+
     }
 }
